abc35869d61e555b7e7e5230ffa123d87377e2f3
Jaroslav Šmolík <grissius@gmail.com>
2017-10-02 19:06:41 +0200

    Add simple toolbar

--- components/Editor.js
+++ components/Editor.js
@@ -24,6 +24,7 @@ class Editor extends React.Component {
             },
         };
         this.handleChange = this.handleChange.bind(this);
+        this.renderBoolOption = this.renderBoolOption.bind(this);
     }
     static propTypes = {
         content: PropTypes.string,
@@ -41,6 +42,20 @@ class Editor extends React.Component {
             html: this.props.toHtml(value),
         });
     }
+    renderBoolOption(name) {
+        const style = this.state.options[name] ? {} : { textDecoration: 'line-through' };
+        return (
+            <span style={style} onClick={() => {
+                this.setState({
+                    options: {
+                        ...this.state.options,
+                        [name]: !this.state.options[name],
+                    }
+                })
+            }}>{name}
+            </span>
+        );
+    };
     render() {
         return (
             <div>
@@ -49,26 +64,39 @@ class Editor extends React.Component {
                     <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Mono" rel="stylesheet" />
                 </Head>
                 <div className="markup-editor">
-                    <CodeMirror value={this.state.raw} onChange={this.handleChange} options={this.state.options} />
-                    <div className="preview" dangerouslySetInnerHTML={{ __html: this.state.html }}></div>
+                    <div className="toolbar">
+                        {this.renderBoolOption('lineNumbers')}
+                        {this.renderBoolOption('lineWrapping')}
+                    </div>
+                    <div className="workspace">
+                        <div className="column">
+                            <CodeMirror value={this.state.raw} onChange={this.handleChange} options={this.state.options} />
+                        </div>
+                        <div className="column">
+                            <div className="preview" dangerouslySetInnerHTML={{ __html: this.state.html }}></div>
+                        </div>
+                    </div>
                 </div>
                 <style jsx global>{`
                 .CodeMirror {
-                  font-family: 'Roboto Mono', monospace;
-                  height: 600px;
+                    font-family: 'Roboto Mono', monospace;
+                    height: 600px;
                 }
-                .markup-editor {
+                .markup-editor .toolbar {
+                    height: 20px;
+                }
+                .preview {
+                    font-family: 'Roboto', sans-serif;
+                    padding: 0 50px 0 20px;
+                }
+                .markup-editor .workspace {
                     align-items: stretch;
                     display: flex;
                     height: 600px;
                     width: 1200px;
                     border: 1px solid rgba(0,0,0,0.3);
                 }
-                .preview {
-                    font-family: 'Roboto', sans-serif;
-                    padding: 0 50px 0 20px;
-                }
-                .markup-editor > * {
+                .markup-editor .workspace > .column {
                     flex: 1;
                     overflow-y: scroll;
                     overflow-x: hidden;

3594f5affeb67c88967bcf0f31142de4f61ac705
Jaroslav Šmolík <grissius@gmail.com>
2017-10-22 16:46:21 +0200

    Use demo doucment as initial value

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import marked from 'marked';
 
 export default class extends React.Component {
     static async getInitialProps() {
-        const res = await fetch(`https://daringfireball.net/projects/markdown/syntax.text`, {
+        const res = await fetch(`http://localhost:3000/static/demo.md`, {
             method: 'GET',
         });
         const markdown = await res.text();

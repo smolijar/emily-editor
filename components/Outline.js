04dd4813eed5f1af03202090b809469a80e812a5
Jaroslav Šmolík <grissius@gmail.com>
2017-10-30 14:12:58 +0100

    Add outline reordable and stackable features

--- components/Outline.js
+++ components/Outline.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import { SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc';
 
 function findHeaders(source, toHtml, headerRegex) {
   const dupIndexMap = {};
@@ -77,22 +77,47 @@ class Outline extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      hidden: {},
     };
   }
   render() {
-    const SortableItem = SortableElement(({ value }) => (
-      <li className="item">
-        <button onClick={() => this.props.onItemClick(value)}>{value.content}</button>
-        <SortableList
-          items={value.children}
-          onSortEnd={indicies => this.props.onOrderChange(value, indicies)}
-        />
-      </li>
-    ));
+    const DragHandle = SortableHandle(() => <span className="dragHandle">::</span>);
+    const SortableItem = SortableElement(({ value }) => {
+      const key = `${value.content}${value.index}`;
+      return (
+        <li className={`item level-${value.level}`}>
+          <button
+            className={`toggle ${value.children.length > 0 ? '' : 'invisible'} ${this.state.hidden[key] ? 'closed' : 'opened'}`}
+            onClick={() => {
+            this.setState({
+                ...this.state,
+                hidden: {
+                  ...this.state.hidden,
+                  [key]: !this.state.hidden[key],
+                },
+              });
+            }}
+          />
+          <DragHandle />
+          <button className="outlineItem" onClick={() => this.props.onItemClick(value)}>{value.content}</button>
+          {!this.state.hidden[key] &&
+          <SortableList
+            items={value.children}
+            onSortEnd={indicies => this.props.onOrderChange(value, indicies)}
+          />
+          }
+        </li>
+      );
+    });
     const SortableList = SortableContainer(({ items }) => (
       <ul className="list">
         {items.map(value => (
-          <SortableItem key={`item-${value.index}`} index={value.index} value={value} />
+          <SortableItem
+            key={`item-${value.index}`}
+            index={value.index}
+            value={value}
+            pressDelay={200}
+          />
         ))}
       </ul>
     ));
@@ -101,28 +126,63 @@ class Outline extends React.Component {
         <SortableList
           items={this.props.outline}
           onSortEnd={indicies => this.props.onOrderChange(null, indicies)}
+          pressDelay={200}
         />
         <style jsx global>{`
             .root {
+              font-family: 'Roboto', sans-serif;
               background-color: #222;
+              color: #ddd;
+              height: 100%;
+            }
+            button:active {
+              outline: 0;
             }
             .list {
               list-style-type: none;
-              // background-color: blue;
               padding-left: 15px;
               margin-bottom: 5px;
               border-left: 1px solid #333;
-              // padding-left: 3px;
+              position: relative;
             }
             .list:first-child {
               margin-top: 0;
               padding-left: 0px;
               border-top: 0;
             }
+            .item:first-child {
+              margin-top: 0;
+            }
             .item {
               margin: 2px 0;
             }
+            .item .dragHandle, .item .toggle {
+              display: inline-block;
+              cursor: pointer;
+              opacity: 0.5;
+            }
+            .item .dragHandle {
+              margin-right: 5px;
+            }
+            .toggle.invisible::before {
+              opacity: 0;
+            }
+            .toggle.opened::before {
+              content: '▼';
+            }
+            .toggle.closed::before {
+              content: '▶';
+              font-size: 0.76em;
+              padding: 0 0.32em;
+            }
+            .item button.outlineItem {
+              width: 100%;
+              position: absolute;
+            }
             .item button {
+              font-weight: bold;
+              cursor: pointer;
+              display: inline-block;
               text-align: left;
               color: #ddd;
               background-color: transparent;
@@ -130,9 +190,14 @@ class Outline extends React.Component {
               white-space: nowrap;
               text-overflow: ellipsis;
               overflow: hidden;
-              width: 100%;
               border: 0;
             }
+            .item.level-1, .item.level-1 button { font-size: 16px; }
+            .item.level-2, .item.level-2 button { font-size: 14px; }
+            .item.level-3, .item.level-3 button { font-size: 12px; }
+            .item.level-4, .item.level-4 button { font-size: 12px; }
+            .item.level-5, .item.level-5 button { font-size: 12px; }
+            .item.level-6, .item.level-6 button { font-size: 12px; }
         `}
         </style>
       </div>

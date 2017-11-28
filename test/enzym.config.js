6af1a8fb7523639cdb3187a680aaf15e29ece96a
Jaroslav Šmolík <grissius@gmail.com>
2017-11-28 13:01:33 +0100

    Mock xhr in jest for typo

--- test/enzym.config.js
+++ test/enzym.config.js
@@ -2,3 +2,11 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+const xhrMockClass = () => ({
+  open: jest.fn(),
+  send: jest.fn(),
+  setRequestHeader: jest.fn(),
+});
+
+window.XMLHttpRequest = jest.fn().mockImplementation(xhrMockClass);

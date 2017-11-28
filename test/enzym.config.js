3f52a613be13f3db1ddc133387e87b7792721af9
Jaroslav Šmolík <grissius@gmail.com>
2017-11-28 18:10:42 +0100

    Add localStorage mock

--- test/enzym.config.js
+++ test/enzym.config.js
@@ -2,3 +2,20 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+const localStorageMock = (() => {
+  let store = {};
+  return {
+    getItem: key => store[key],
+    setItem: (key, value) => {
+      store[key] = value.toString();
+    },
+    clear: () => {
+      store = {};
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+  };
+})();
+global.localStorage = localStorageMock;

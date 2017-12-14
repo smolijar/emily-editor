6b698cb0aceb93b43420a3ce77c267407125ef57
Jaroslav Šmolík <grissius@gmail.com>
2017-12-14 11:05:29 +0100

    Fix markdown header regex
    Require beginning at the start of line
    Allow single character for eql/dash underline syntax

--- src/modes/markdown.js
+++ src/modes/markdown.js
@@ -67,7 +67,7 @@ const markdown = {
     return line.replace(/(.*)(\w)(.*)/, `$1$2${content}$3`);
   },
   // must include newline after
-  headerRegex: /(#+\s+\S.*\n)|(\S.*\n(===+|---+)\n)/g,
+  headerRegex: /(\n|^)(#+\s+\S.*\n)|(\S.*\n(=+|-+)\n)/g,
   renderJsxStyle: () => (
     <style jsx global>{`
       .markdown-body {

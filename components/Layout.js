b20795fd8dc57cf97a65d45dba0be20ae4344a22
Jaroslav Šmolík <grissius@gmail.com>
2018-02-28 15:16:30 +0100

    Add FA for asciidoc
    For adoc to use fa, one must at the moment add `:icons: font` atop document.

--- components/Layout.js
+++ components/Layout.js
@@ -20,6 +20,7 @@ class Layout extends React.PureComponent {
           <script src="ace/src-min/ace.js" />
           <link rel="stylesheet" type="text/css" href="hljs/styles/github.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.9.0/github-markdown.min.css" />
+          <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
         </Head>
         <Link href="/">
           <button>Home</button>

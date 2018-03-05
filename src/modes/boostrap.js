c6925bfe85a178c25d55b18c4738422263c9d3d8
Jaroslav Šmolík <grissius@gmail.com>
2018-03-05 16:54:02 +0100

    Add mode default optional props to bootstrap

--- src/modes/boostrap.js
+++ src/modes/boostrap.js
@@ -1,3 +1,14 @@
+import _ from 'lodash';
+
 export default (mode) => {
-  return mode;
+  const fn = _ => _;
+  const defaultSet = {
+    toHtml: fn,
+    postProcess: fn,
+    lineSafeInsert: (line, content) => `${line} ${content}`,
+    renderJsxStyle: () => {},
+    previewClassName: '',
+  };
+
+  return _.merge(defaultSet, mode);
 }

da43864f4be126fa35da0157a579b760233805a3
Jaroslav Šmolík <grissius@gmail.com>
2018-03-07 17:24:59 +0100

    Refactor value expiration

--- src/components/editor/autosave.js
+++ src/components/editor/autosave.js
@@ -1,28 +1,25 @@
 import _ from 'lodash';
 import md5 from 'md5';
 
-const MAX_AGE = 86400000;
 const PREFIX = 'emily-backup-';
 
 const getKey = (env = '') => {
   return `${PREFIX}${md5(env)}`;
 };
 
-
 const cleanup = () => _.entries(localStorage)
   .filter(([key]) => key.startsWith(PREFIX))
   .forEach(([key, val]) => {
-    const age = new Date() - new Date(JSON.parse(val).date);
-    if (age > MAX_AGE) {
-      console.warn(key);
+    if (new Date() >= new Date(JSON.parse(val).ttd)) {
       localStorage.removeItem(key);
     }
   });
 
-export const autosaveStore = (value, env) => {
+export const autosaveStore = (value, env, ttl = 86400000) => {
   const store = {
     value,
     date: new Date(),
+    ttd: new Date(new Date().getTime() + ttl),
   };
   localStorage.setItem(getKey(env), JSON.stringify(store));
   return store;

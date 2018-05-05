fad4a9acec373365d722bf78955b3d1676bff2d4
Jaroslav Šmolík <grissius@gmail.com>
2018-05-05 15:34:13 +0200

    Export generateMode function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const markdown = require('./dist/modes/markdown').default;
 const asciidoc = require('./dist/modes/asciidoc').default;
+const generateMode = require('./dist/modes/generateMode').default;
 const Emily = require('./dist/components/Editor');
 
 module.exports = Emily;
 module.exports.modes = { markdown, asciidoc };
+module.exports.generateMode = generateMode;

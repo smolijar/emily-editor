0903b6b822a44b2641e5bbd6dc894235bcebcc9b
Jaroslav Šmolík <grissius@gmail.com>
2018-05-08 20:14:58 +0200

    Fix export

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const markdown = require('./dist/modes/markdown').default;
 const asciidoc = require('./dist/modes/asciidoc').default;
-const generateMode = require('./dist/modes/generateMode').default;
+const generateMode = require('./dist/modes/generateMode');
 const Emily = require('./dist/components/Editor');
 
 module.exports = Emily;
